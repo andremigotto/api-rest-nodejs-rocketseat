643c02129f54c78072c8ef06aa4afb14a83d2098
Migotto <andremigotto@icloud.com>
2024-05-13 19:24:25 -0300

    Adding coockies on app, middlaware for checking session_id user and adding for preHandler

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
+import cookie from "@fastify/cookie"
 
 const app = fastify()
 
+//Adding cookie for register
+app.register(cookie)
+
+//Adding routes for register
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
